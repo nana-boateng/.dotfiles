75fac730e1c56b05d9f5a20a86ebc2b2b558a013
Nana Boateng <n.boateng@utoronto.ca>
2022-09-14 11:33:34 -0400

    Update nvim config

--- nvim/lua/nana/keymaps.lua
+++ nvim/lua/nana/keymaps.lua
@@ -28,12 +28,19 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Close buffers
+keymap("n", "<leader>c", ":bw<CR>", opts)
+
+-- Tabs
+keymap("n", "<leader>n", ":tabn<CR>", opts)
+keymap("n", "<leader>p", ":tabp<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
