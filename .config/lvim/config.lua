4c45b0f5be5332ac931c5c9322112376452ffcf9
nana-boateng <n.boateng@utoronto.ca>
2023-07-14 11:51:04 -0400

    lvim update, other stuff updated probs

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,24 +1,106 @@
--- General
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Neovim
+-- =========================================
+lvim.leader = " "
+lvim.colorscheme = "tokyonight" -- set to a custom theme
+lvim.builtin.time_based_themes = true -- set false to use your own configured theme
+lvim.transparent_window = true -- enable/disable transparency
+lvim.debug = false
+vim.lsp.set_log_level "error"
 lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "catppuccin"
+lvim.lsp.code_lens_refresh = true
+lvim.lsp.installer.setup.automatic_installation = false
+require("user.neovim").config()
 
--- Keymappings
-lvim.leader = "space"
+-- Customization
+-- =========================================
+lvim.builtin.sell_your_soul_to_devil = { active = false, prada = false } -- if you want microsoft to abuse your soul
+lvim.builtin.lastplace = { active = false } -- change to false if you are jumping to future
+lvim.builtin.tabnine = { active = true } -- change to false if you don't like tabnine
+lvim.builtin.persistence = { active = true } -- change to false if you don't want persistence
+lvim.builtin.presence = { active = true } -- change to true if you want discord presence
+lvim.builtin.orgmode = { active = false } -- change to true if you want orgmode.nvim
+lvim.builtin.dap.active = true -- change this to enable/disable debugging
+lvim.builtin.fancy_statusline = { active = true } -- enable/disable fancy statusline
+lvim.builtin.fancy_wild_menu = { active = false } -- enable/disable cmp-cmdline
+lvim.builtin.fancy_diff = { active = true } -- enable/disable fancier git diff
+lvim.builtin.lua_dev = { active = true } -- change this to enable/disable folke/lua_dev
+lvim.builtin.test_runner = { active = true, runner = "ultest" } -- change this to enable/disable ultest or neotest
+lvim.builtin.cheat = { active = false } -- enable/disable cheat.sh integration
+lvim.builtin.sql_integration = { active = false } -- use sql integration
+lvim.builtin.smooth_scroll = "cinnamon" -- for smoth scrolling, can be "cinnamon", "neoscroll" or ""
+lvim.builtin.neoclip = { active = true, enable_persistent_history = false }
+lvim.builtin.nonumber_unfocus = false -- diffrentiate between focused and non focused windows
+lvim.builtin.custom_web_devicons = false -- install https://github.com/Nguyen-Hoang-Nam/mini-file-icons
+lvim.builtin.harpoon = { active = false } -- use the harpoon plugin
+lvim.builtin.remote_dev = { active = false } -- enable/disable remote development
+lvim.builtin.cursorline = { active = true } -- use a bit fancier cursorline
+lvim.builtin.motion_provider = "hop" -- change this to use different motion providers ( hop or leap )
+lvim.builtin.hlslens = { active = false } -- enable/disable hlslens
+lvim.builtin.csv_support = false -- enable/disable csv support
+lvim.builtin.sidebar = { active = false } -- enable/disable sidebar
+lvim.builtin.task_runner = "" -- change this to use different task runners ( "async_tasks" or "overseer" or "")
+lvim.builtin.winbar_provider = "filename" -- can be "filename" or "treesitter" or "navic" or ""
+lvim.builtin.metals = {
+  active = false, -- enable/disable nvim-metals for scala development
+  fallbackScalaVersion = "3.2.0-RC3",
+  serverVersion = "0.11.12",
+}
+lvim.builtin.collaborative_editing = { active = false } -- enable/disable collaborative editing
+lvim.builtin.file_browser = { active = false } -- enable/disable telescope file browser
+lvim.builtin.sniprun = { active = false } -- enable/disable sniprun
+lvim.builtin.tag_provider = "symbols-outline" -- change this to use different tag providers ( symbols-outline or vista )
+lvim.builtin.global_statusline = false -- set true to use global statusline
+lvim.builtin.dressing = { active = false } -- enable to override vim.ui.input and vim.ui.select with telescope
+lvim.builtin.refactoring = { active = false } -- enable to use refactoring.nvim code_actions
+lvim.builtin.tmux_lualine = false -- use vim-tpipeline to integrate lualine and tmux
+lvim.builtin.lsp_lines = false -- enable/disable lsp_lines to display lsp virtual text below instead of behind
+lvim.builtin.legendary = { active = false } -- enable/disable legendary plugin ( ctrl-p command )
+lvim.builtin.tree_provider = "nvimtree" -- can be "neo-tree" or "nvimtree" or ""
+lvim.builtin.lir.active = false
+lvim.builtin.breadcrumbs.active = false
+lvim.builtin.illuminate.active = false
+lvim.builtin.indentlines.active = true
+lvim.builtin.noice = { active = true } -- enables noice.nvim (notifications) and inc-rename.nvim (refractor vars w/ hl)
+lvim.builtin.go_programming = { active = false } -- gopher.nvim + nvim-dap-go
+lvim.builtin.python_programming = { active = false } -- swenv.nvim + nvim-dap-python + requirements.txt.vim
+lvim.builtin.web_programming = { active = true } -- typescript.nvim + package-info.nvim
+lvim.builtin.rust_programming = { active = false } -- rust_tools.nvim + crates.nvim
+lvim.builtin.cpp_programming = { active = false } -- clangd_extensions.nvim + make-tools.nvim
+lvim.builtin.cmp.cmdline.enable = false
+lvim.builtin.borderless_cmp = false
+lvim.builtin.colored_args = true -- if true then sets up hlargs.nvim
+lvim.builtin.bigfile.active = true
 
--- Ctrl-S to save
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- Override Lunarvim defaults
+-- =========================================
+require("user.builtin").config()
 
--- Bufferline
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- Additional Plugins
+-- =========================================
+require("user.plugins").config()
 
---
-vim.api.nvim_set_keymap("n", "<C-\\>", ":ToggleTerm<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", '<C-\\>', "<c-\\><c-n>:ToggleTerm<CR>", { noremap = true, silent = true })
+-- Autocommands
+-- =========================================
+require("user.autocommands").config()
 
--- External Files
-require("nana.plugins")
-require("nana.builtin")
-require("nana.theme")
-require("nana.lsp")
+-- Additional Keybindings
+-- =========================================
+require("user.keybindings").config()
+
+-- StatusLine
+-- =========================================
+if lvim.builtin.fancy_statusline.active then
+  require("user.lualine").config()
+end
+
+-- Debugging
+-- =========================================
+if lvim.builtin.dap.active then
+  require("user.dap").config()
+end
+
